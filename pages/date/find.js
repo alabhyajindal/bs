f37c4ee24ed55e065f6f8e7084eaf456be8b4b2e
Alabhya Jindal <alabhya10@gmail.com>
2023-03-22 01:19:43 +0530

    Styling

--- pages/date/find.js
+++ pages/date/find.js
@@ -1,12 +1,13 @@
 export default function Find() {
   return (
     <div className='flex flex-col gap-4 mt-8 max-w-md'>
-      <p>
-        We are now looking for a match based on your preferences. You will get
-        an email with your date details by{' '}
-        <span className='underline'>9pm, Thursday</span>.
+      <p className='text-lg'>
+        We&apos;re currently working on finding a match that fits your
+        preferences. By <span className='underline'>Thursday at 9 PM</span>,
+        you&apos;ll receive an email with all the details for your date,
+        including the time and restaurant location.
       </p>
-      <p>
+      <p className='text-lg'>
         Please remember that match making depends on the people available for
         dates matching your preferences, so you may or may not get a date this
         week.{' '}

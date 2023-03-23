febf33c100542e2fb3bbfcc6aff0bb4da0e4ba80
Alabhya Jindal <alabhya10@gmail.com>
2023-03-23 20:17:33 +0530

    head

--- pages/date/start.js
+++ pages/date/start.js
@@ -1,17 +1,24 @@
+import Head from 'next/head';
+
 export default function Start() {
   return (
-    <div className='flex flex-col gap-4 mt-8 max-w-md'>
-      <p className='text-lg'>
-        We&apos;re currently working on finding a match that fits your
-        preferences. By <span className='underline'>Thursday at 9 PM</span>,
-        you&apos;ll receive an email with all the details for your date,
-        including the time and restaurant location.
-      </p>
-      <p className='text-lg'>
-        Please remember that match making depends on the people available for
-        dates matching your preferences, so you may or may not get a date this
-        week.{' '}
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Start | Blind Saturday</title>
+      </Head>
+      <div className='flex flex-col gap-4 mt-8 max-w-md'>
+        <p className='text-lg'>
+          We&apos;re currently working on finding a match that fits your
+          preferences. By <span className='underline'>Thursday at 9 PM</span>,
+          you&apos;ll receive an email with all the details for your date,
+          including the time and restaurant location.
+        </p>
+        <p className='text-lg'>
+          Please remember that match making depends on the people available for
+          dates matching your preferences, so you may or may not get a date this
+          week.{' '}
+        </p>
+      </div>
+    </>
   );
 }
